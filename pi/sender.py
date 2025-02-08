5677935ef9ca300281ea24d912d8af0718d32c08
loulou <lpt.louisphilippe@gmail.com>
2025-02-08 18:04:28 -0500

    small changes

--- pi/sender.py
+++ pi/sender.py
@@ -2,11 +2,37 @@ import asyncio
 import cv2
 import json
 import socketio
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack, AudioStreamTrack
 from av import VideoFrame
 import fractions
+import numpy as np
+import pyaudio
 
 sio = socketio.AsyncClient()  # Socket.IO Client
+
+class CustomAudioStreamTrack(AudioStreamTrack):
+    def __init__(self, rate=22050, channels=1):
+        super().__init__()
+        self.rate = rate
+        self.channels = channels
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format=pyaudio.paInt16,
+            channels=self.channels,
+            rate=self.rate,
+            input=True,
+            frames_per_buffer=960
+        )
+
+    async def recv(self):
+        audio_data = self.stream.read(960, exception_on_overflow=False)
+        audio_array = np.frombuffer(audio_data, dtype=np.int16)
+
+        audio_frame = AudioFrame(format="s16", layout="mono", samples=960)
+        audio_frame.planes[0].update(audio_array.tobytes())
+        audio_frame.pts = None  # Leave `None` to let aiortc handle timestamps
+        audio_frame.sample_rate = self.rate
+        return audio_frame
 
 class CustomVideoStreamTrack(VideoStreamTrack):
     def __init__(self, camera_id):
@@ -29,7 +55,9 @@ class CustomVideoStreamTrack(VideoStreamTrack):
 async def setup_webrtc():
     pc = RTCPeerConnection()
     video_sender = CustomVideoStreamTrack(0)
+    audio_sender = CustomAudioStreamTrack()
     pc.addTrack(video_sender)
+    pc.addTrack(audio_sender)
 
     @pc.on("icecandidate")
     async def on_icecandidate(candidate):
